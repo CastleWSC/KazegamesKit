b5e37f37dc7dc0ae813bb225c100441e005b7c7f
Castle <castle.wsc@gmail.com>
2020-05-05 11:49:36 +0800

    update to 1.1.2

---
+++ Editor/KazegamesEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+
+namespace KazegamesKit.Editor
+{
+    public class KazegamesEditor 
+    {
+        [MenuItem("Tools/Kazegames/Create Prototype Folders")]
+        static void CreatePrototypeFolders()
+        {
+            if(!AssetDatabase.IsValidFolder(Path.Combine(Application.dataPath, "_Prototype")))
+            {
+                string[] subFolders = new string[]
+                {
+                    "Scripts",
+                    "Scenes",
+                    "Textures",
+                    "Materials",
+                    "Fonts",
+                    "Prefabs"
+                };
+
+
+                string root = AssetDatabase.CreateFolder("Assets", "_Prototype");
+                
+                for(int i=0; i<subFolders.Length; i++)
+                {
+                    AssetDatabase.CreateFolder("Assets/_Prototype", subFolders[i]);
+                }
+
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+}
