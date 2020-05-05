d552274bfc1ab86d1a3c061a31d0269a8fd93bea
Castle <castle.wsc@gmail.com>
2020-05-05 13:54:20 +0800

    update to 1.1.3
    modify load scene task

--- Runtime/Task/SceneTask.cs
+++ Runtime/Task/SceneTask.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Net.NetworkInformation;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -17,7 +15,29 @@ namespace KazegamesKit
 
         public static SceneTask CreateTask(string sceneName, EOperation operation, bool autoStart = true)
         {
-            SceneTask newTask = new SceneTask(sceneName, operation);
+            var scene = SceneManager.GetSceneByName(sceneName);
+
+            if (scene == null)
+            {
+                throw new NullReferenceException("SceneTask: Not found the scene by name= " + sceneName);
+            }
+
+            SceneTask newTask = new SceneTask(scene, operation);
+            if (autoStart) newTask.Start();
+
+            return newTask;
+        }
+
+        public static SceneTask CreateTask(int buildIndex, EOperation operation, bool autoStart = true)
+        {
+            var scene = SceneManager.GetSceneByBuildIndex(buildIndex);
+
+            if (scene == null)
+            {
+                throw new NullReferenceException("SceneTask: Not found the scene by build index= " + buildIndex);
+            }
+
+            SceneTask newTask = new SceneTask(scene, operation);
             if (autoStart) newTask.Start();
 
             return newTask;
@@ -26,17 +46,18 @@ namespace KazegamesKit
 
         private AsyncOperation _async;
 
-        private string _name;
+        private Scene _scene;
         private EOperation _op;
         private float _progress;
 
-        public string SceneName { get { return _name; } }
+        public string SceneName { get { return _scene.name; } }
         public EOperation Operation { get { return _op; } }
         public float Progress { get { return _progress; } }
 
-        private SceneTask(string name, EOperation op)
+
+        private SceneTask(Scene scene, EOperation op)
         {
-            _name = name;
+            _scene = scene;
             _op = op;
         }
 
@@ -64,11 +85,11 @@ namespace KazegamesKit
 
                 if (_op == EOperation.Unload)
                 {
-                    _async = SceneManager.UnloadSceneAsync(_name);
+                    _async = SceneManager.UnloadSceneAsync(_scene);
                 }
                 else
                 {
-                    _async = SceneManager.LoadSceneAsync(_name, _op == EOperation.LoadSingle ? LoadSceneMode.Single : LoadSceneMode.Additive);
+                    _async = SceneManager.LoadSceneAsync(_scene.buildIndex, _op == EOperation.LoadSingle ? LoadSceneMode.Single : LoadSceneMode.Additive);
 
                     _async.allowSceneActivation = false;
                 }
